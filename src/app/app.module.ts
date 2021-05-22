a9366ffe438e72717e66ae99aafbc82a75bc8f7d
AzharKhanPlay <azharkhanplay@gmail.com>
2021-05-22 20:17:20 +0530

    Build for Prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ServicesComponent } from './services/services.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { ExamplesComponent } from './examples/examples.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ReviewComponent } from './review/review.component';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServicesComponent,
+    ExamplesComponent,
+    PricingComponent,
+    ContactUsComponent,
+    ReviewComponent,
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
   ],
-  providers: [],
+  exports: [
+  
+  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
