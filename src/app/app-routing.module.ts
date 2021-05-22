b6091d81ba51a610e3b78ae8bff99bc7459702a6
AzharKhanPlay <azharkhanplay@gmail.com>
2021-05-22 20:34:13 +0530

    Reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ExamplesComponent } from './examples/examples.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'services', component: ServicesComponent },
+  { path:'services/:page', component: ServicesComponent },
+  { path: 'examples', component: ExamplesComponent },
+  { path:'contact-us', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
